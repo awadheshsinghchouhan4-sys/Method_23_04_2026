89d803da669215f7fba22a5ff1c13e9b67c964ad
Awadhesh Singh Chouhan <awadhesh.chouhan@acuvate.com>
2026-04-23 16:13:42 +0530

    file task9 method

---
+++ methods23-04-2026/method-23-04-2026-taskQ8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods23_04_2026
+{
+    internal class method_23_04_2026_taskQ8
+    {
+
+
+        public static void StudentDetails(int subject1, int subject2, int subject3, out int total, out int avg )
+        {
+            total = 0;
+            avg = 0;
+            total = subject1 + subject2 + subject3;
+            avg = total / 3;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write(" Enter the mark of subject 1 :");
+            int subjectmark1= Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Enter the mark of subject 2 :");
+            int subjectmark2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Enter the mark of subject 3 :");
+            int subjectmark3 = Convert.ToInt32(Console.ReadLine());
+
+            int total;
+            int avg;
+            StudentDetails(subjectmark1, subjectmark2, subjectmark3, out total, out avg);
+
+            Console.WriteLine($" Average  mark is {avg} and the total marks is {total}");
+        }
+    }
+}
---
+++ methods23-04-2026/methodtaskQ9-23-04-2026.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods23_04_2026
+{
+    internal class methodtaskQ9_23_04_2026
+    {
+        public static void CalculateDiscount(int amount, out double sgst,out double cgst)
+        {
+            sgst = amount * 0.03;
+            cgst = amount * 0.05;
+
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter the Amount of the Customer :");
+            int amount=Convert.ToInt32(Console.ReadLine());
+
+            double totalAmount = 0;
+            double cgstTotalAmount = 0;
+            double sgstTotalAmount = 0;
+
+            CalculateDiscount(amount, out sgstTotalAmount, out cgstTotalAmount);
+
+            totalAmount = amount + sgstTotalAmount + cgstTotalAmount;
+            Console.WriteLine("The Cgst Charge is :"+ cgstTotalAmount);
+            Console.WriteLine("The Cgst Charge is :"+ sgstTotalAmount);
+            Console.WriteLine( $" The total charge is : {totalAmount}");
+
+
+        }
+    }
+}
