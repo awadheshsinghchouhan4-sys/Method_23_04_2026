d3ef14eda6803f5eda4413198254ba79e7e12dfb
awadhesh.chouhan <awadhesh.chouhan@ACU-HYD-LT-1909>
2026-04-23 11:25:58 +0530

    Add project files.

---
+++ methods23-04-2026/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods23_04_2026
+{
+    internal class Program
+    {
+        public static void Wish()
+        {
+            Console.WriteLine("hello wishing");
+        }
+
+
+        // methods with parameters and return value 
+
+        static int Add(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            // accecing the value to the instance
+
+            Program p = new Program();
+            //p.Wish();
+
+            //calling through the static method
+
+            //Wish();
+            // void will not return the the value
+            // it oly used for printing value
+
+            // creating the objext of the methods 
+
+            int sum=Add(10,20);
+            Console.WriteLine("The sum is :"+sum);
+            Console.ReadKey();
+        }
+
+
+    }
+}
